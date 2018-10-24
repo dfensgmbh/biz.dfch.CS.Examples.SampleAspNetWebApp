ee27ab96ed82af2cabd7db9cde1a8f1d063c74ab
rufer7 <rufermarc@gmail.com>
2018-10-24 11:16:08 +0200

    Initial commit

---
+++ src/biz.dfch.CS.Examples.SampleAspNetWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(biz.dfch.CS.Examples.SampleAspNetWebApp.Startup))]
+namespace biz.dfch.CS.Examples.SampleAspNetWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
